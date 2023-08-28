b5edaa39331e919076e0f8b9940a7fe982640ceb
chenyue404 <601450225@qq.com>
2023-08-28 17:25:44 +0800

    自动添加拦截器

--- debugger/src/main/java/zerobranch/androidremotedebugger/AndroidRemoteDebugger.java
+++ debugger/src/main/java/zerobranch/androidremotedebugger/AndroidRemoteDebugger.java
@@ -22,6 +22,7 @@ import org.jetbrains.annotations.NotNull;
 
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
+import okhttp3.OkHttpClient;
 import top.canyie.pine.PineConfig;
 import zerobranch.androidremotedebugger.logging.DefaultLogger;
 import zerobranch.androidremotedebugger.logging.Logger;
@@ -60,17 +61,18 @@ public final class AndroidRemoteDebugger {
         PineConfig.debug = true; // 是否debug，true会输出较详细log
         PineConfig.debuggable = BuildConfig.DEBUG; // 该应用是否可调试，建议和配置文件中的值保持一致，否则会出现问题
 
-        XposedHelpers.findAndHookMethod("okhttp3.OkHttpClient.Builder",
-                androidRemoteDebugger.getClass().getClassLoader(),
-                "build",
+        XposedHelpers.findAndHookConstructor(
+                OkHttpClient.class,
+                OkHttpClient.Builder.class,
                 new XC_MethodHook() {
                     @Override
                     protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        XposedHelpers.callMethod(param.thisObject,
-                                "addInterceptor",
-                                new NetLoggingInterceptor());
+                        OkHttpClient.Builder builder = (OkHttpClient.Builder) param.args[0];
+                        builder.addInterceptor(new NetLoggingInterceptor());
+                        android.util.Log.d("123123", "beforeHookedMethod: ");
                     }
-                });
+                }
+        );
 
         if (!isEnable) {
             stop();
